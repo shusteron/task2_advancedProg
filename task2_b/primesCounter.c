f0bd6c871b4e065dd757aff87a6b816b58ceee29
ron_shuster <118674717+shusteron@users.noreply.github.com>
2024-06-30 03:17:27 +0300

    Add files via upload

--- task2_b/primesCounter.c
+++ task2_b/primesCounter.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -7,63 +6,52 @@
 #define NUM_THREADS 16
 #define CHUNK_SIZE 128 
 
-pthread_mutex_t queueMutex;
-pthread_cond_t queueCond;
-pthread_mutex_t primeCountMutex;
-int primeCount = 0;
+pthread_mutex_t queueMutex;  // Mutex for protecting access to the queue
+pthread_cond_t queueCond;    // Condition variable for signaling the queue
+pthread_mutex_t primeCountMutex;  // Mutex for protecting access to the prime count
+int primeCount = 0;  // Global counter for prime numbers
 
+// Structure representing a chunk of numbers
 typedef struct {
     int numbers[CHUNK_SIZE];
     int size;
 } Chunk;
 
+// Node structure for the linked list queue
 typedef struct Node {
     Chunk chunk;
     struct Node *next;
 } Node;
 
+// Queue structure for holding chunks
 typedef struct {
     Node *head;
     Node *tail;
     int size;
 } Queue;
 
-Queue queue = {NULL, NULL, 0};
+Queue queue = {NULL, NULL, 0};  // Initialize an empty queue
 
-
-// Original isPrime
-// bool isPrime(int n) {
-//     if (n <= 1) {
-//         return false;
-//     }
-//     for (int i = 2; i * i <= n; i++) {
-//         if (n % i == 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-
+// Optimized isPrime function
 bool isPrime(int n) {
-    if (n <= 1) return false;
-    if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    // we already know that 4 is not prime so we can start from 5
+    if (n <= 1) return false;  // 0 and 1 are not prime numbers
+    if (n <= 3) return true;   // 2 and 3 are prime numbers
+    if (n % 2 == 0 || n % 3 == 0) return false;  // Eliminate multiples of 2 and 3
+
+    // Check for factors from 5 onwards, skipping even numbers
     for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) return false;
     }
     return true;
 }
 
-
-
+// Function to enqueue a chunk into the queue
 void enqueue(Chunk chunk) {
-    Node *newNode = (Node *)malloc(sizeof(Node));
+    Node *newNode = (Node *)malloc(sizeof(Node));  // Allocate memory for a new node
     newNode->chunk = chunk;
     newNode->next = NULL;
 
-    pthread_mutex_lock(&queueMutex);
+    pthread_mutex_lock(&queueMutex);  // Lock the queue mutex
     if (queue.tail) {
         queue.tail->next = newNode;
     } else {
@@ -71,14 +59,15 @@ void enqueue(Chunk chunk) {
     }
     queue.tail = newNode;
     queue.size++;
-    pthread_cond_signal(&queueCond);
-    pthread_mutex_unlock(&queueMutex);
+    pthread_cond_signal(&queueCond);  // Signal that a new item is added to the queue
+    pthread_mutex_unlock(&queueMutex);  // Unlock the queue mutex
 }
 
+// Function to dequeue a chunk from the queue
 Chunk dequeue() {
-    pthread_mutex_lock(&queueMutex);
+    pthread_mutex_lock(&queueMutex);  // Lock the queue mutex
     while (queue.size == 0) {
-        pthread_cond_wait(&queueCond, &queueMutex);
+        pthread_cond_wait(&queueCond, &queueMutex);  // Wait for an item to be enqueued
     }
 
     Node *node = queue.head;
@@ -89,15 +78,16 @@ Chunk dequeue() {
     queue.size--;
 
     Chunk chunk = node->chunk;
-    free(node);
-    pthread_mutex_unlock(&queueMutex);
+    free(node);  // Free the memory of the dequeued node
+    pthread_mutex_unlock(&queueMutex);  // Unlock the queue mutex
     return chunk;
 }
 
+// Function executed by each thread to process numbers
 void *processNumbers(void *arg) {
     while (1) {
-        Chunk chunk = dequeue();
-        if (chunk.size == 0) break; // End signal
+        Chunk chunk = dequeue();  // Dequeue a chunk of numbers
+        if (chunk.size == 0) break;  // End signal
 
         int localPrimeCount = 0;
         for (int i = 0; i < chunk.size; ++i) {
@@ -106,9 +96,9 @@ void *processNumbers(void *arg) {
             }
         }
 
-        pthread_mutex_lock(&primeCountMutex);
+        pthread_mutex_lock(&primeCountMutex);  // Lock the prime count mutex
         primeCount += localPrimeCount;
-        pthread_mutex_unlock(&primeCountMutex);
+        pthread_mutex_unlock(&primeCountMutex);  // Unlock the prime count mutex
     }
     return NULL;
 }
@@ -136,7 +126,7 @@ int main() {
         enqueue(chunk);
     }
 
-    // Enqueue end signals
+    // Enqueue end signals to stop all threads
     for (unsigned int i = 0; i < NUM_THREADS; ++i) {
         Chunk endSignal = {{0}, 0};
         enqueue(endSignal);
